52d7bf60f4718834ef0010bfaaa65762b836917f
Nico Schneider <nschneid@1-A-18.42heilbronn.de>
2025-03-13 18:33:19 +0100

    added telescope and smaller changes

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,40 +1,16 @@
 -- ************************************************************************** --
 --                                                                            --
 --                                                        :::      ::::::::   --
---   init.lua                                           :+:      :+:    :+:   --
+--   telescope.lua                                      :+:      :+:    :+:   --
 --                                                    +:+ +:+         +:+     --
 --   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
---   Created: 2025/03/10 14:32:07 by nschneid          #+#    #+#             --
---   Updated: 2025/03/13 11:54:42 by nschneid         ###   ########.fr       --
+--   Created: 2025/03/13 11:59:13 by nschneid          #+#    #+#             --
+--   Updated: 2025/03/13 12:00:29 by nschneid         ###   ########.fr       --
 --                                                                            --
 -- ************************************************************************** --
 
-vim.g.mapleader = "<Space>"
-require("config.lazy")
-
-vim.cmd("colorscheme cyberdream")
-
-local opt = {
-	number = true,
-	relativenumber = true,
-
-	expandtab = false,
-	shiftwidth = 4,
-	tabstop = 4,
-	softtabstop = 4,
-
-
-	colorcolumn = "80",
-
-	list = true,
-	clipboard = "unnamedplus",
-	incsearch = true,
-	hlsearch = true
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' }
 }
-
-for k, v in pairs(opt) do
-	vim.opt[k] = v
-end
-
-vim.opt.listchars = {tab = '>•'}
